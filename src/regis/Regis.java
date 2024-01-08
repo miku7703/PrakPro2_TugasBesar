e886071dc2e91895ced33d86bbf5680a690db32c
Adam-Adr12123 <96764165+Adam-Adr12123@users.noreply.github.com>
2024-01-08 14:06:41 +0700

    Frame Awal dari E-runtah

---
+++ src/regis/Regis.java
@@ -0,0 +1,19 @@
+
+package regis;
+
+
+public class Regis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+         
+    }
+    
+}
